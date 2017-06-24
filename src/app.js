5d9cf21639ebfe47e6bee35754727fb3d78a8ca6
jiji55 <juhoop@gmail.com>
2017-06-25 01:10:17 +0300

    more of figuring things out

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 // have to start somewhere..
+/*
+* TODO
+* - insert images
+* - controls?
+* - docs?
+*/
 
 class App {
 
-	constructor(width, height){
-		
+	constructor( width, height ){
 		var canvas = document.createElement( 'canvas' );
 		canvas.width = width;
 		canvas.height = height;
 		document.body.appendChild( canvas );
 		this.context = canvas.getContext( "2d" );
-
 	}
+
+	// ?
+	add_images( image_source ){
+		this.img = new Image();
+		this.img.src = image_source;
+	}
+
+	draw_image( image, x, y ){
+		//this.context.drawImage( image, x, y ) }; ?
+	}
+
 }
