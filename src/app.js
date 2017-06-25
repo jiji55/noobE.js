2fdae5542a1d05fcf84ac72e42291c5bad053b8c
jiji55 <juhoop@gmail.com>
2017-06-26 00:25:58 +0300

    image loader that seems to work + mousepos + rect click func

--- src/app.js
+++ src/app.js
@@ -3,27 +3,73 @@
 * TODO
 * - insert images
 * - controls?
-* - docs?
 */
 
 class App {
 
 	constructor( width, height ){
-		var canvas = document.createElement( 'canvas' );
-		canvas.width = width;
-		canvas.height = height;
-		document.body.appendChild( canvas );
-		this.context = canvas.getContext( "2d" );
+		this.canvas = document.createElement( 'canvas' );
+		this.canvas.width = width;
+		this.canvas.height = height;
+		document.body.appendChild( this.canvas );
+		this.context = this.canvas.getContext( "2d" );
 	}
 
-	// ?
-	add_images( image_source ){
-		this.img = new Image();
-		this.img.src = image_source;
+	load_images( images ){
+		//convert single img to array
+		if ( images.length === "undefined" ){
+			images = [images];
+		}
+
+		var count = images.length;
+
+		var whenCompleted = function ( images, i ){
+			count--;
+			console.log(count);
+			if ( count == 0 ){
+				console.log("All done");
+			}
+		}
+
+		for ( var i = 0; i < images.length; i++ ){
+			this.loadImg( images, i, whenCompleted )
+		}
+	}
+
+	loadImg( images, i, whenCompleted ) {
+
+		// (e) is the event object that gets passed down
+		var whenLoaded = function(e){
+		e.target.removeEventListener( "load", whenLoaded );
+		whenCompleted( images, i )
+	}
+
+		var img = new Image();
+		img.addEventListener( "load", whenLoaded );
+		img.src = images[i];
 	}
 
 	draw_image( image, x, y ){
-		//this.context.drawImage( image, x, y ) }; ?
+		this.context.drawImage( image, x, y );
 	}
 
+	getMousePos( canvas, event ){
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: event.clientX - rect.left,
+			y: event.clientY - rect.top
+		};
+	}
+
+	clickInsideBox( rect, mousePos ){
+		if ( rect.x < mousePos.x && 
+			 rect.x + rect.width > mousePos.x  && 
+			 rect.y < mousePos.y &&
+			 rect.y + rect.height > mousePos.y ){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
